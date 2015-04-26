a859ddf77eaa7e91263c4bea29465e40d4c5408d
Dainis Gorbunovs <'dainis.gorbunovs@student.manchester.ac.uk'>
2015-04-26 10:59:23 +0100

    Muse code for measuring eyebrow movement

---
+++ README.md
@@ -0,0 +1,16 @@
+Pre-preparation: pair your Muse with your device via Bluetooth.
+Starting Muse takes 1s.
+
+1. Launch muse-io:
+muse-io --preset 14 --osc osc.udp://localhost:5006
+
+2. Run the Twilio nodejs server
+
+3. Change the server address in painMuse and then launch painMuse:
+node painMuse.js
+
+Might need to install osc first:
+npm install osc
+
+4. Wear Muse tightly to the forehead. Rise eyebrows as you are suprised.
+Eyebrows naturally rise when you are in pain.
---
+++ painMuse.js
@@ -0,0 +1,86 @@
+var osc = require("osc");
+var http = require('http');
+var querystring = require('querystring');
+
+logAction("Started PainMuse.");
+
+// Create an osc.js UDP Port listening on port 57121. 
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 5006
+});
+
+var active = false;
+
+// ***** EDIT THIS SERVER ADDRESS *****
+var twilioAddress = '81959c8a.ngrok.io';
+
+// Listen for incoming OSC bundles. 
+udpPort.on("message", function (oscBundle) {
+	switch(oscBundle.address)
+	{
+		case "/muse/config":
+			logAction(JSON.parse(oscBundle.args[0]).battery_percent_remaining + "% battery");
+			break;
+		case "/muse/eeg/quantization":
+			// rise the eyebrows up
+			if (oscBundle.args[1] > 8 && oscBundle.args[2] > 8)
+			{
+				//logAction("Surprised");
+	    		if (!active)
+	    		{
+	    			// post a message
+		    		logAction("Posting message to support");
+		    		postCode("Need help");
+		    		logAction("Sent a message to support");
+	    		}
+	    		active = true;
+		    }
+		    else
+	    		active = false;
+			break;
+	}
+});
+
+function postCode(codestring) {
+  // Build the post string from an object
+  var post_data = querystring.stringify({
+      'compilation_level' : 'ADVANCED_OPTIMIZATIONS',
+      'output_format': 'json',
+      'output_info': 'compiled_code',
+        'warning_level' : 'QUIET',
+        'js_code' : codestring
+  });
+
+  // An object of options to indicate where to post to
+  var post_options = {
+      host: twilioAddress,
+      port: '80',
+      path: '/',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': post_data.length
+      }
+  };
+
+  // Set up the request
+  var post_req = http.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('Response: ' + chunk);
+      });
+  });
+
+  // post the data
+  post_req.write(post_data);
+  post_req.end();
+}
+
+function logAction(action)
+{
+	console.log(new Date(), ":", action);
+}
+
+// Open the socket. 
+udpPort.open();
